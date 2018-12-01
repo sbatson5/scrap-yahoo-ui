ccf97f54ebd4911f8949898fbac1ec74eda243f8
Scott Batson <sjbats@email.com>
2018-12-01 14:34:29 -0500

    add stats card

---
+++ src/components/matchup-stats.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function matchUpStats({ matchups }) {
+
+  const matchupsByScore = matchups.sort((a, b) => a.score - b.score);
+
+  const victoryScores = matchupsByScore
+    .filter((matchup) => matchup.victory)
+    .map((matchup) => parseFloat(matchup.score));
+
+  const lossScores = matchupsByScore
+    .filter((matchup) => !matchup.victory)
+    .map((matchup) => parseFloat(matchup.score));
+
+  const lowestVictory = victoryScores[0];
+  const highestVictory = victoryScores.reverse()[0];
+  const lowestLoss = lossScores[0];
+  const highestLoss = lossScores.reverse()[0];
+
+  return (
+    <div>
+      <p>Highest Victory: {highestVictory}</p>
+      <p>Lowest Victory: {lowestVictory}</p>
+      <p>Highest Loss: {highestLoss}</p>
+      <p>Lowest Loss: {lowestLoss}</p>
+    </div>
+  );
+}
--- src/routes/view-manager.js
+++ src/routes/view-manager.js
@@ -3,6 +3,7 @@ import fetchManagerById from '../utils/fetch-manager-by-id';
 import fetchMatchups from '../utils/fetch-matchups';
 import fetchAllSeasons from '../utils/fetch-all-seasons';
 import ManagerCard from '../components/manager-card';
+import MatchupStats from '../components/matchup-stats';
 
 export default function manager({ match }) {
   const [user, setUser] = useState({});
@@ -29,20 +30,15 @@ export default function manager({ match }) {
   if (!user.id) getManager();
   if (matchups.length < 1 && user.id) getMatchups();
 
-  const getWins = function(seasonId) {
-    console.log(seasonId);
-    let filteredMatchups = matchups;
-    if (seasonId) {
-      filteredMatchups = filteredMatchups.filter((matchup) => matchup.season_id === seasonId);
-    }
+  const matchupsBySeason = (seasonId) => matchups.filter((matchup) => matchup.season_id === seasonId);
+
+  const getWins = (seasonId) => {
+    let filteredMatchups = seasonId ? matchupsBySeason(seasonId) : matchups;
     return filteredMatchups.filter((matchup) => matchup.victory).length;
   }
 
-  const getLosses = function(seasonId) {
-    let filteredMatchups = matchups;
-    if (seasonId) {
-      filteredMatchups = filteredMatchups.filter((matchup) => matchup.season_id === seasonId);
-    }
+  const getLosses = (seasonId) => {
+    let filteredMatchups = seasonId ? matchupsBySeason(seasonId) : matchups;
     return filteredMatchups.filter((matchup) => !matchup.victory).length;
   }
 
@@ -58,6 +54,7 @@ export default function manager({ match }) {
             <h3 key={season.id}>{season.year}</h3>
             <p key="wins-{season.id}">Wins: {getWins(season.id)}</p>
             <p key="losses-{season.id}">Losses: {getLosses(season.id)}</p>
+            <MatchupStats matchups={matchupsBySeason(season.id)} />
           </div>
         )
       })}
